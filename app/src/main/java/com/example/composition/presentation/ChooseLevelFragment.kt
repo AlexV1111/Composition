7132916bfadebc4aee69420ce1e84a86e5d61f96
Alex <Alexvvv9511@yandex.ru>
2025-08-11 22:36:44 +0300

    add navigation from FragmentManager

--- app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import com.example.composition.R
 import com.example.composition.databinding.FragmentChooseLevelBinding
 import com.example.composition.databinding.FragmentGameFinishedBinding
+import com.example.composition.domain.entity.Level
 
 class ChooseLevelFragment : Fragment() {
 
@@ -24,9 +25,40 @@ class ChooseLevelFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        clickLevelButton()
+    }
+
+    private fun clickLevelButton() {
+        with(binding) {
+            buttonLevelTest.setOnClickListener { launchGameFragment(Level.TEST) }
+            buttonLevelEasy.setOnClickListener { launchGameFragment(Level.EASY) }
+            buttonLevelNormal.setOnClickListener { launchGameFragment(Level.NORMAL) }
+            buttonLevelHard.setOnClickListener { launchGameFragment(Level.HARD) }
+        }
+
+    }
+
+    private fun launchGameFragment(level: Level) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFragment.newInstance(level))
+            .addToBackStack(GameFragment.NAME)
+            .commit()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+
+        const val NAME = "ChooseLevelFragment"
+
+        fun newInstance(): ChooseLevelFragment {
+            return ChooseLevelFragment()
+        }
     }
 
 }
