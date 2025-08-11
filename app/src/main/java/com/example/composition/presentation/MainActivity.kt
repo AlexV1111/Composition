fe86139c464c06cd743b0d282e3eff2d57cce251
Alex <Alexvvv9511@yandex.ru>
2025-08-11 15:06:44 +0300

    make domain layer

---
+++ app/src/main/java/com/example/composition/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.composition.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.composition.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
