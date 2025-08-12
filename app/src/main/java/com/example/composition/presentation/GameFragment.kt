1d68ef8a053d5d39006780c6711cab1a02888488
Alex <Alexvvv9511@yandex.ru>
2025-08-12 13:02:55 +0300

    make GameViewModel

--- app/src/main/java/com/example/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -1,18 +1,21 @@
 package com.example.composition.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.composition.R
 import com.example.composition.databinding.FragmentGameBinding
 import com.example.composition.domain.entity.GameResult
-import com.example.composition.domain.entity.GameSettings
 import com.example.composition.domain.entity.Level
 
 
 class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
 
     private lateinit var level: Level
 
@@ -35,13 +38,9 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvSum.setOnClickListener {
-            launchGameFinishedFragment(
-                GameResult(
-                    true, 10, 12, GameSettings(9, 9, 9, 30)
-                )
-            )
-        }
+
+        viewModel = ViewModelProvider(this)[GameViewModel::class.java]
+
     }
 
     override fun onDestroyView() {
@@ -53,11 +52,14 @@ class GameFragment : Fragment() {
         requireArguments().getParcelable<Level>(KEY_LEVEL)?.let { level = it }
     }
 
-    private fun launchGameFinishedFragment(gameResult: GameResult) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
-            .addToBackStack(null)
-            .commit()
+    private fun launchGameFinishedFragment(gameResult: GameResult, timerIsFinish: Boolean) {
+
+        if (timerIsFinish) {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
 
